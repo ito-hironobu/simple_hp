beb26bdc899f7e10d96dcf56808da27f120c9ca8
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-09-08 12:29:52 +0900

    「接続しました。」と表示されるのを削除
    削除しました。

--- php/preserve_counter.php
+++ php/preserve_counter.php
@@ -12,7 +12,6 @@ try{
 	$pdo = new PDO($dsn, $db_user, $db_pass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-	print "接続しました。";
 }catch(PDOException $Exception){
 	die('接続エラー：'. $Exception->getMessage());
 }
